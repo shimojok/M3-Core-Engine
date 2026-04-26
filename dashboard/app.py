20b5dcc6f276e8cc60f2173d75a3382494c37c33
shimojok <shimojok@terraviss.com>
2026-04-26 21:30:22 +0900

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -136,9 +136,7 @@ with col2:
 with col3:
     st.markdown("### 🔬 Why It Matters")
 
-    
     st.info("**The Triple-Currency Hypercycle**")
-    
     st.markdown("""
     | Currency | Molecule | Function |
     |---|---|---|
